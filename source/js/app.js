c4d301f23746add64cc0092f6358ab0f8004fa0a
Chris Ashton <Chris.Ashton@bbc.co.uk>
2014-08-21 13:29:14 +0100

    sharetools update

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,4 @@
-define(['lib/news_special/bootstrap', 'lib/news_special/share_tools/controller', 'module/app'], function (news, shareTools, App) {
+define(['lib/news_special/bootstrap', 'lib/news_special/share_tools/controller', 'module/app', 'module/journey'], function (news, shareTools, App, journey) {
 
     return {
         init: function (storyPageUrl) {
@@ -8,7 +8,7 @@ define(['lib/news_special/bootstrap', 'lib/news_special/share_tools/controller',
             news.$('#newsspec_5380').addClass('ns__' + view).removeClass('noJs');
 
             shareTools.init('.main', {
-                storyPageUrl: 'http://www.bbc.co.uk/news/science-environment-22567526',
+                storyPageUrl: 'http://bbc.in/14WYuxQ',
                 header:       'Share this page',
                 message:      'BBC News - Secret life of the cat: What do our feline companions get up to?',
                 hashtag:      'horizoncats',
@@ -19,6 +19,19 @@ define(['lib/news_special/bootstrap', 'lib/news_special/share_tools/controller',
 
             // fix for mobile - want share tools to appear BELOW content
             news.$('.ns__mobile .share').appendTo(news.$('.ns__mobile'));
+
+            news.pubsub.on('moveToPosition', updateShare);
+            news.pubsub.on('topnavItemClicked', updateShare);
+
+            function updateShare(position) {
+                if (view === 'desktop') {
+                    var cat = journey['point_' + position].header,
+                        shareMessage = 'What DO cats get up to? I followed ' + cat + ' to find out. How about you?';
+
+                    news.pubsub.emit('ns:share:message', [shareMessage]);
+                    news.$('.ns__desktop .share .share__button p').html("Share " + cat + "'s journey");
+                }
+            }
 
             news.sendMessageToremoveLoadingImage();
         }
